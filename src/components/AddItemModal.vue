2700c9608bd220c410ff00fa6a0cfce94d3fc5a3
faizan <faizan-666@outlook.com>
2021-09-28 22:20:47 +0500

    Added an Add item Modal

---
+++ src/components/AddItemModal.vue
@@ -0,0 +1,32 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Add Item</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Add Item</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      
+      <ion-button @click="closeModal">Click me to close modal</ion-button>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, modalController } from '@ionic/vue';
+
+export default  {
+  name: 'AddItemModal',
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
+  methods: {
+    async closeModal() {
+      await modalController.dismiss()
+    }
+  }
+}
+</script>
--- src/views/Vault.vue
+++ src/views/Vault.vue
@@ -5,8 +5,8 @@
         <ion-searchbar></ion-searchbar>
         <!-- eslint-disable -->
         <ion-buttons slot="end">
-          <ion-button>
-          <ion-icon :icon="addOutline" />
+          <ion-button @click="setOpen(true)">
+            <ion-icon :icon="addOutline" />
           </ion-button>
         </ion-buttons>
       </ion-toolbar>
@@ -17,12 +17,15 @@
           <ion-title size="large">Vault</ion-title>
         </ion-toolbar>
       </ion-header>
+      <ion-modal :is-open="isOpenRef" @didDismiss="setOpen(false)">
+        <add-item-modal />
+      </ion-modal>
       <categories-my-vault />
     </ion-content>
   </ion-page>
 </template>
 
-<script lang="ts">
+<script>
 import {
   IonPage,
   IonHeader,
@@ -33,10 +36,13 @@ import {
   IonButton,
   IonButtons,
   IonSearchbar,
+  IonModal,
 } from "@ionic/vue";
 import CategoriesMyVault from "@/components/CategoriesMyVault.vue";
+import AddItemModal from "@/components/AddItemModal.vue";
+
 // import ExploreContainer from "@/components/ExploreContainer.vue";
-import { addOutline } from 'ionicons/icons';
+import { addOutline } from "ionicons/icons";
 
 export default {
   name: "Vault",
@@ -50,13 +56,21 @@ export default {
     IonButton,
     IonButtons,
     IonSearchbar,
+    IonModal,
     IonPage,
     CategoriesMyVault,
+    AddItemModal,
   },
   data() {
     return {
-      addOutline
-    }
-  }
+      addOutline,
+      isOpenRef: false,
+    };
+  },
+  methods: {
+    setOpen(flag) {
+      this.isOpenRef = flag;
+    },
+  },
 };
 </script>
